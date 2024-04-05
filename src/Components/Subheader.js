f0ece7a5d4b460f202ffb8db158e59976b2e3834
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-05 15:51:27 +0700

    create Subheader in components

---
+++ src/Components/Subheader.js
@@ -0,0 +1,18 @@
+export default function Subheader() {
+    return (
+        <section className="topDes container">
+            <p>Top Vacation Destinations</p>
+            <div className="card-group gap-3">
+            <div className="card">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBPJjijOP71IW4YtsHw9f8ALjWeq8OLYYyTCdcAEx-3w&s" className="card-img-top" alt="..." />
+            </div>
+            <div className="card">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBPJjijOP71IW4YtsHw9f8ALjWeq8OLYYyTCdcAEx-3w&s" className="card-img-top" alt="..." />
+            </div>
+            <div className="card">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBPJjijOP71IW4YtsHw9f8ALjWeq8OLYYyTCdcAEx-3w&s" className="card-img-top" alt="..." />
+            </div>
+            </div>
+        </section>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Layout from "@/Components/Layout";
 import Header from "@/Components/Header";
+import Subheader from "@/Components/Subheader";
 
 export default function Home() {
   return (
     <Layout>
       <Header />
+      <Subheader />
     </Layout>
   )
 }
