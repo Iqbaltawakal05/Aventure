6a467df93b72cf7d9fc16c79353080b77aa79579
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-05 16:01:36 +0700

    Add login html and css

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,7 +1,26 @@
 export default function Login() {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-page">
+        <div className="login-header">
+            <img src='/wave.svg' alt="login icon"/>
+        </div>
+        <div className="login-body">
+            <div className="login-container">
+                <img src='/authIcon.svg' alt="login icon"/>
+            <div className="login-form">
+                <h1>Login</h1>
+                {/* {!!notif.length && <h5>{notif}</h5>} */}
+                <input type="text" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+                <button type="submit" className="login-btn">Login</button>
+                <button type="submit" className="back-btn" onClick={() => window.history.back()}>Back</button>
+                <div className="link-register">
+                    <p>Don't have an account?</p>
+                        <a href="/register">register</a>
+                </div>
+            </div>
+        </div>
+        </div>
     </div>
   )
 }
