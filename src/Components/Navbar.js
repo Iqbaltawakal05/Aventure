2f23de8c3aef47a3b683d190268e6280c1270373
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-05 15:29:20 +0700

    Add html to navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,28 @@
+
 export default function Navbar() {
+
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav className="navbar navbar-expand-lg" id="navbar">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">Event</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Features</a>
+                    </li>
+                    <li className="nav-item">
+                    <a className="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li className="nav-item">
+                            <a href='/login' className="nav-link">Login</a>
+                    </li>
+                    <li className="nav-item">
+                            <a href='/register' className="nav-link">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
