df3013566811fe6f741a9541321eabdc4fd0d34c
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-05 15:44:27 +0700

    create img in layout.js and add css

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -4,6 +4,9 @@ import Navbar from "./Navbar";
 export default function Layout({ children }) {
     return (
         <div>
+            <div className="headerImg">
+                <img src="/header.jpg" alt="placeholder" />
+            </div>
             <Navbar />
             {children}
             <Footer />
