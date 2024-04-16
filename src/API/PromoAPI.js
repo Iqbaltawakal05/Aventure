0e0dcce180830c2fcdb8b012b3fa45c8df235330
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-17 06:54:12 +0700

    Add folder API and call API AllPromo

---
+++ src/API/PromoAPI.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+async function fetchAllPromoData() {
+    try {
+        const response = await axios.get('https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/promos', 
+        {
+            headers: {
+                apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
+            }
+        });
+        console.log(response.data)
+        return response.data.data;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        return null;
+    }
+}
---
+++ src/Components/_Promo.js
@@ -0,0 +1,7 @@
+export default function Promo() {
+    return (
+        <div className="promo container">
+            <h1>Promo</h1>
+        </div>
+    )
+}
