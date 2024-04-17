6e61026acafecdf9a5202bfbc32808a29178a62e
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-17 07:16:52 +0700

    Add promo page

--- src/API/PromoAPI.js
+++ src/API/PromoAPI.js
@@ -16,4 +16,20 @@ async function fetchAllPromoData() {
     }
 }
 
-export { fetchAllPromoData }
+async function fetchPromoById(id) {
+    try {
+        const response = await axios.get(`https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/promo/${id}`,
+        {
+            headers: {
+                apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
+            }
+        });
+        console.log(response.data)
+        return response.data.data;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        return null;
+    }
+}
+
+export { fetchAllPromoData, fetchPromoById }
---
+++ src/pages/promo/index.js
@@ -0,0 +1,23 @@
+import { fetchAllPromoData } from "@/API/PromoAPI";
+import { useEffect, useState } from "react";
+
+export default function Promo() {
+    const [promos, setPromos] = useState([]);
+
+    useEffect(() => {
+        fetchAllPromoData().then((data) => {
+            setPromos(data);
+        });
+    }, []);
+
+    return (
+        <div>
+            {promos.map((promo) => (
+                <div key={promo.id}>
+                    <h1>{promo.title}</h1>
+                    <p>{promo.description}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
