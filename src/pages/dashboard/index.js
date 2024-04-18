0572e145b694ac21c6966cd24ab378a475bacbe3
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-18 19:57:20 +0700

    show dashboardNavbar to dashboard page and add css

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,23 +1,11 @@
 import { LogoutData } from "@/API/AuthAPI";
+import Dashboardnavbar from "@/Components/DashboardNav";
 
 export default function Dashboard() {
 
-    const handleLogout = async () => {
-        try {
-            const res = await LogoutData();
-            localStorage.removeItem('token');
-            window.location.href = '/';
-            console.log(res);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
     return (
-        <div>
+        <Dashboardnavbar>
             <h1>Dashboard</h1>
-            <p>Dashboard content here</p>
-            <button onClick={handleLogout}>Logout</button>
-        </div>
+        </Dashboardnavbar>
     )
 }
