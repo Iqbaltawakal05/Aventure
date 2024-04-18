5d452ae728ea9153f4a0e0250e751c166ed4bbe2
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-18 20:06:41 +0700

    success call allUser API to userpage in dashboard

--- src/Components/DashboardLayout.js
+++ src/Components/DashboardLayout.js
@@ -1,6 +1,6 @@
 import Dashboardnavbar from "./DashboardNav";
 
-export default function LayoutDash({ children }) {
+export default function DashboardLayout({ children }) {
     return (
         <div className="text-center row">
             <div className="col-2">
---
+++ src/pages/dashboard/user.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { fetchAllUserData } from '@/API/UserAPI';
+import DashboardLayout from '@/Components/DashboardLayout';
+
+
+export default function User() {
+    const [userData, setUserData] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetchAllUserData();
+            setUserData(data);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <DashboardLayout>
+            <div className="text-center">
+                <div>
+                    <h1>User</h1>
+                    <div className="alluser row">
+                            <div className='col-2'>
+                                <p>profile</p>
+                            </div>
+                            <div className="col-1">
+                                <p>name</p>
+                            </div>
+                            <div className='col-3'>
+                            <p>email</p>
+                            </div>
+                            <div className='col-2'>
+                            <p>role</p>
+                            </div>
+                            <div className='col-3'>
+                            <p>action</p>
+                            </div>
+                        </div>
+                     {userData && userData.data && userData.data.map((user, index) => (
+                        <div className="alluser row" key={index}>
+                            <div className='col-2'>
+                                <img src={user.profilePictureUrl} alt={`${user.name} profile`}/>
+                            </div>
+                            <div className="col-1">
+                                <p>{user.name}</p>
+                            </div>
+                            <div className='col-3'>
+                            <p>{user.email}</p>
+                            </div>
+                            <div className='col-2'>
+                            <p>{user.role}</p>
+                            </div>
+                            <div className='col-3'>
+                                <div className="dropdown">
+                                    <button className="btn btn-secondary dropdown-toggle" type="button" id={`dropdownMenuButton-${index}`} data-bs-toggle="dropdown" aria-expanded="false">
+                                        Edit role
+                                    </button>
+                                    <ul className="dropdown-menu" aria-labelledby={`dropdownMenuButton-${index}`}>
+                                        <li><button className="dropdown-item">Admin</button></li>
+                                        <li><button className="dropdown-item">User</button></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </DashboardLayout>
+    );
+}
