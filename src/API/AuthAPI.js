181744a57d6cfd25320111fb480652eb65426a7d
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-18 11:53:05 +0700

    Call login API and success set token in localStorage

---
+++ src/API/AuthAPI.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+async function LoginData(email, password, setLoading) {
+    try {
+        const res = await axios.post('https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/login',
+        {
+            email,
+            password,
+        },
+        {
+            headers: {
+                apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
+                'Content-Type': 'application/json',
+            }
+        });
+
+        localStorage.setItem('token', res.data.token);
+
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            window.location.href = '/dashboard';
+        }
+        alert(res.data.message);
+        setTimeout(() => {
+                setLoading(false);
+            }, 2000);
+        } catch (error) {
+            alert(error.response.data.message);
+            setLoading(false);
+        }
+    };
+
+export { LoginData }
--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,27 +1,42 @@
+import { LoginData } from "@/API/AuthAPI";
+import React, { useState, useEffect } from "react";
 export default function Login() {
-    
-  return (
-    <div className="login-page">
-        <div className="login-header">
-            <img src='/wave.svg' alt="login icon"/>
-        </div>
-        <div className="login-body">
-            <div className="login-container">
-                <img src='/authIcon.svg' alt="login icon"/>
-            <div className="login-form">
-                <h1>Login</h1>
-                {/* {!!notif.length && <h5>{notif}</h5>} */}
-                <input type="text" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
-                <button type="submit" className="login-btn">Login</button>
-                <button type="submit" className="back-btn" onClick={() => window.history.back()}>Back</button>
-                <div className="link-register">
-                    <p>Don't have an account?</p>
-                        <a href="/register">register</a>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await LoginData(email, password);
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    return (
+        <div className="login-page">
+            <div className="login-header">
+                <img src='/wave.svg' alt="login icon"/>
+            </div>
+            <div className="login-body">
+                <div className="login-container">
+                    <img src='/authIcon.svg' alt="login icon"/>
+                    <div className="login-form">
+                        <h1>Login</h1>
+                        <form onSubmit={handleSubmit}>
+                            <input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                            <button type="submit" className="login-btn">Login</button>
+                        </form>
+                        <button type="button" className="back-btn" onClick={() => window.history.back()}>Back</button>
+                        <div className="link-register">
+                            <p>Don't have an account?</p>
+                            <a href="/register">register</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        </div>
-    </div>
-  )
+    );
 }
