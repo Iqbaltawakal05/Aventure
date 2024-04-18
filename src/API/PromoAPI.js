d3580bfdc35555b452854a1c76e31ae738005558
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-18 20:36:28 +0700

    call updateUser API

--- src/API/PromoAPI.js
+++ src/API/PromoAPI.js
@@ -31,4 +31,21 @@ async function fetchPromoById(id) {
     }
 }
 
-export { fetchAllPromoData, fetchPromoById }
+async function updatePromo(id, promoData) {
+    try {
+        const response = await axios.post(`https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/update-promo/${id}`, promoData, {
+            headers: {
+                apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.error('Error updating promo:', error);
+        return null;
+    }
+}
+
+export { fetchAllPromoData, fetchPromoById, updatePromo }
