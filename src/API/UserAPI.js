9e05f461008dec11fdf18faf10d1548e60088939
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-18 20:00:40 +0700

    create userAPI file and call API Alluser

---
+++ src/API/UserAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+async function fetchAllUserData() {
+    try {
+        const response = await axios.get('https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/all-user', 
+        {
+            headers: {
+                apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
+                Authorization: `bearer ${localStorage.getItem('token')}`,
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        return null;
+    }
+}
+
+export { fetchAllUserData }
