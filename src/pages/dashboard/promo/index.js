6807989f9b2f00f28e94e7f835358a744f3a21f0
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-18 20:18:08 +0700

    call Allpromo API to dashboard promo

---
+++ src/pages/dashboard/promo/index.js
@@ -0,0 +1,41 @@
+import { fetchAllPromoData } from '@/API/PromoAPI';
+import { useEffect, useState } from "react";
+import Link from 'next/link';
+import DashboardLayout from "@/Components/DashboardLayout";
+
+export default function Promo() {
+    const [promoData, setPromoData] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetchAllPromoData();
+            setPromoData(data);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <DashboardLayout>
+            <div className="text-center">
+                <Link href="/dashboard/promo/createPromo">
+                <button>Create</button>
+                </Link>
+                {promoData && promoData.map((promo, index) => (
+                    <div className="row" key={index}>
+                        <img src={promo.imageUrl} alt={promo.title} />
+                        <p>{promo.title}</p>
+                        <p>{promo.description}</p>
+                        <p>{promo.promo_code}</p>
+                        <div>
+                            <Link href={`/dashboard/promo/${promo.id}`} passHref>
+                                <button type="button" className="btn btn-primary">
+                                    View Details
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </DashboardLayout>
+    );
+}
