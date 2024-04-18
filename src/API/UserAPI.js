ba867453df1476eb0b2e73d50a0f158fd94caaea
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-18 20:08:33 +0700

    call updateUser API in userAPI

--- src/API/UserAPI.js
+++ src/API/UserAPI.js
@@ -16,4 +16,20 @@ async function fetchAllUserData() {
     }
 }
 
-export { fetchAllUserData }
+async function updateUserRole(userId, newRole) {
+  try {
+    const response = await axios.post(`https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/update-user-role/${userId}`, { role: newRole },
+    {
+      headers: {
+        apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
+        Authorization: `bearer ${localStorage.getItem('token')}`,
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error updating user role:', error);
+    throw error;
+  }
+}
+
+export { fetchAllUserData, updateUserRole }
