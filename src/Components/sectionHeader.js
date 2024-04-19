b8d88efbc02bf1995afa4c5eeb00b9e38e002fe7
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-20 06:14:00 +0700

    add icon in sectionHeader and add css

--- src/Components/sectionHeader.js
+++ src/Components/sectionHeader.js
@@ -1,10 +1,21 @@
+import Link from "next/link";
+
 export default function SectionHeader() {
     return (
         <section className="navbar-nav links">
                 <div className="links-item">
-                    <a className="Country-links" aria-current="page" href="/category">Country</a>
-                    <a className="Offer-links" href="/promo">Offers</a>
-                    <a className="Vacations-links" href="/activity">Vacations</a>
+                    <div className="Country-link">
+                        <i className="bi bi-buildings-fill"></i>
+                        <a className="Country-links">Country</a>
+                    </div>
+                    <div className="Offer-link">
+                        <i className="bi bi-cash-coin"></i>
+                        <a className="Offer-links" href="/promo">Offers</a>
+                    </div>
+                    <div className="Vacations-link">
+                        <i className="bi bi-backpack2-fill"></i>
+                        <a className="Vacations-links" href="/activity">Vacations</a>
+                    </div>
                 </div>
         </section>
     )
