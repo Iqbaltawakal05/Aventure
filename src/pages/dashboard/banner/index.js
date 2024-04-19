86c311949ad6c183f74443d6dde56cd6ee68a98d
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 07:29:19 +0700

    call AllBanner API  to dashbooard banner page

--- src/pages/dashboard/banner/index.js
+++ src/pages/dashboard/banner/index.js
@@ -1,9 +1,39 @@
+import { fetchAllBannersData } from "@/API/BannerAPI";
 import DashboardLayout from "@/Components/DashboardLayout";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Banner() {
+    const [banner, setBanner] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetchAllBannersData();
+            setBanner(data);
+        }
+        fetchData();
+    }, []);
+
     return (
         <DashboardLayout>
-            <h1>Banner</h1>
+            <div className="text-center">
+                <Link href="/dashboard/banner/create">
+                <button>Create</button>
+                </Link>
+                {banner && banner.map((banner, index) => (
+                    <div className="row" key={index}>
+                        <img src={banner.imageUrl} alt={banner.name} />
+                        <p>{banner.name}</p>
+                        <div>
+                            <Link href={`/dashboard/banner/${banner.id}`} passHref>
+                                <button type="button" className="btn btn-primary">
+                                    View Details
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </DashboardLayout>
     )
 }
