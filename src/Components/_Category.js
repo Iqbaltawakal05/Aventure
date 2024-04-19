6cb0c2dd8579d1d149d83c66aba0916c40dab555
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 16:49:17 +0700

    install react-slick and slick-carousel

--- src/Components/_Category.js
+++ src/Components/_Category.js
@@ -19,17 +19,15 @@ export default function Category() {
     }, []);
 
     return (
-        <section className="topDes container">
-            <p>Top Vacation Destinations</p>
-            <div className="card-group gap-3">
-            {category.map((category, index) => (
+        <section className="Country container">
+            <h1>Country</h1>
+             {category.map((category, index) => (
                 <div className="card" key={category.id}>
                     <img src={category.imageUrl} alt={category.name} />
                     <p>{category.name}</p>
                     <Link href={`/category/${category.id}`}><button className="category-button">detail</button></Link>
                 </div>
             ))}
-            </div>
         </section>
     )
 }
