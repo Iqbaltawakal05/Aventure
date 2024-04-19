e48c30226f61e36d0293da0f12fb9518cb0e4a76
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 15:33:58 +0700

    create sectionHeader and import to homepage

---
+++ src/Components/sectionHeader.js
@@ -0,0 +1,7 @@
+export default function SectionHeader() {
+    return (
+        <section>
+            <h1>href</h1>
+        </section>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import Promo from "@/Components/_Promo";
 import Banner from "@/Components/_Banner";
 import Category from "@/Components/_Category";
 import Activity from "@/Components/_Activity";
+import SectionHeader from "@/Components/sectionHeader";
 
 export default function Home() {
   return (
     <Layout>
       <Header />
+      <SectionHeader />
       <Category />
       <Promo />
       <Banner />
