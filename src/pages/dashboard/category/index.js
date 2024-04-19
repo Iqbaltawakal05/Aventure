4ebcb0ce3b7dc3e85aa35bf9bacb4088a5bb87b1
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 08:09:42 +0700

    call AllCategory API to dashboard  categorypage

--- src/pages/dashboard/category/index.js
+++ src/pages/dashboard/category/index.js
@@ -1,9 +1,39 @@
+import { fetchAllCategoriesData } from "@/API/CategoryAPI";
 import DashboardLayout from "@/Components/DashboardLayout";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Category() {
+    const [category, setCategory] = useState([]);
+
+    useEffect (() => {
+        async function fetchData() {
+            const data = await fetchAllCategoriesData();
+            setCategory(data);
+        }
+        fetchData();
+
+    })
     return (
         <DashboardLayout>
-            <h1>Category</h1>
+            <div className="text-center">
+                <Link href="/dashboard/category/create">
+                <button>Create</button>
+                </Link>
+                {category && category.map((category, index) => (
+                    <div className="row" key={index}>
+                        <img src={category.imageUrl} alt={category.name} />
+                        <p>{category.name}</p>
+                        <div>
+                            <Link href={`/dashboard/category/${category.id}`} passHref>
+                                <button type="button" className="btn btn-primary">
+                                    View Details
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </DashboardLayout>
     )
 }
