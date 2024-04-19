ee26bd8292fb5892f4c8d6a36d9101bd7504d462
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 12:20:29 +0700

    call deleteActivity API

--- src/API/ActivityAPI.js
+++ src/API/ActivityAPI.js
@@ -8,7 +8,6 @@ async function fetchAllActivitiesData() {
                 apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
             }
         });
-        console.log(response.data.data)
         return response.data.data;
     } catch (error) {
         console.error('Error fetching user data:', error);
@@ -24,7 +23,6 @@ async function fetchActivityById(id) {
                 apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
             }
         })
-        console.log(response.data);
         return response.data.data;
     } catch (error) {
         console.error('Error fetching user data:', error);
@@ -48,4 +46,21 @@ async function updateActivity(id, activityData) {
     }
 }
 
-export { fetchAllActivitiesData, fetchActivityById, updateActivity }
+async function deleteActivity(id) {
+    try {
+        const response = await axios.delete(`https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/delete-activity/${id}`,
+         {
+            headers: {
+                apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            }
+        });
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.error('Error updating promo:', error);
+        return null;
+    }
+}
+
+export { fetchAllActivitiesData, fetchActivityById, updateActivity, deleteActivity }
