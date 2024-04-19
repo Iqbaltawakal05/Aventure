0d0a99fa49b4bcad54b0e5db016b355e84b277d9
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 10:28:01 +0700

    call updateActivity API

--- src/API/ActivityAPI.js
+++ src/API/ActivityAPI.js
@@ -32,4 +32,20 @@ async function fetchActivityById(id) {
     }
 }
 
-export { fetchAllActivitiesData, fetchActivityById }
+async function updateActivity(id, activityData) {
+    try {
+        const response = await axios.post(`https://travel-journal-api-bootcamp.do.dibimbing.id/api/v1/update-activity/${id}`, activityData, {
+            headers: {
+                apiKey: '24405e01-fbc1-45a5-9f5a-be13afcd757c',
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+                'Content-Type': 'application/json'
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error updating promo:', error);
+        return null;
+    }
+}
+
+export { fetchAllActivitiesData, fetchActivityById, updateActivity }
