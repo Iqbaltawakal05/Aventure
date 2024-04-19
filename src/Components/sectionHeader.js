1083fb5637f0fb8c78d698ce6942ee7d6ccad8f0
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 16:19:01 +0700

    change code in sectionHeader and add css sectionHeader

--- src/Components/sectionHeader.js
+++ src/Components/sectionHeader.js
@@ -1,17 +1,11 @@
 export default function SectionHeader() {
     return (
-        <section>
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/category">Category</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/promo">Promo</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/activity">Vacations</a>
-                </li>
-            </ul>
+        <section className="navbar-nav links">
+                <div className="links-item">
+                    <a className="Country-links" aria-current="page" href="/category">Country</a>
+                    <a className="Offer-links" href="/promo">Offers</a>
+                    <a className="Vacations-links" href="/activity">Vacations</a>
+                </div>
         </section>
     )
 }
