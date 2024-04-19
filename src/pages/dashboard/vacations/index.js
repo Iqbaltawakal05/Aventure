c127702e9e40b01fe3ff2c49aee996cf2d0c93cc
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 09:56:25 +0700

    call AllActivity API to dashboard activityPage

--- src/pages/dashboard/vacations/index.js
+++ src/pages/dashboard/vacations/index.js
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
+import Link from 'next/link';
+import DashboardLayout from "@/Components/DashboardLayout";
+import { fetchAllActivitiesData } from "@/API/ActivityAPI";
+
 export default function Vacations() {
+    const [vacations, setVacations] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await fetchAllActivitiesData();
+            setVacations(data);
+        }
+        fetchData();
+    }, []);
+
     return (
-        <div>
-            <h1>Vacations</h1>
-        </div>
-    )
+        <DashboardLayout>
+            <div className="text-center">
+                <Link href="/dashboard/vacations/create">
+                <button>Create</button>
+                </Link>
+                {vacations && vacations.map((vacations, index) => (
+                    <div className="row" key={index}>
+                        <img src={vacations.imageUrls} alt={vacations.title} />
+                        <p>{vacations.title}</p>
+                        <p>{vacations.description}</p>
+                        <div>
+                            <Link href={`/dashboard/vacations/${vacations.id}`} passHref>
+                                <button type="button" className="btn btn-primary">
+                                    View Details
+                                </button>
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </DashboardLayout>
+    );
 }
