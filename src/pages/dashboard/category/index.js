37abb7f8411ad855c2c51ee0d5358a9f00c25e34
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 08:05:03 +0700

    create dashboard categoryPage

---
+++ src/pages/dashboard/category/index.js
@@ -0,0 +1,9 @@
+import DashboardLayout from "@/Components/DashboardLayout";
+
+export default function Category() {
+    return (
+        <DashboardLayout>
+            <h1>Category</h1>
+        </DashboardLayout>
+    )
+}
