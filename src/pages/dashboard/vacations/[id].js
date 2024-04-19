05fa1cb2b6ec48fdc2c41ae6306627324607688e
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 10:24:37 +0700

    success call DetailVacations API in dashboard id vacations page

--- src/pages/dashboard/vacations/[id].js
+++ src/pages/dashboard/vacations/[id].js
@@ -1,7 +1,43 @@
+import { fetchActivityById } from "@/API/ActivityAPI";
+import DashboardLayout from "@/Components/DashboardLayout";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 export default function VacationDetail() {
+    const router = useRouter();
+    const { id } = router.query;
+    const [vacations, setVacations] = useState([]);
+
+    useEffect (() => {
+        async function fetchData() {
+            const data = await fetchActivityById(id);
+            setVacations(data);
+        }
+        if (id) {
+            fetchData();
+        }
+    }, [id]);
+
     return (
-        <div>
-            <h1>Vacation Detail</h1>
-        </div>
+        <DashboardLayout>
+            <div className="text-center">
+                <img src={vacations.imageUrls} />
+                <p>{vacations.title}</p>
+                <p>{vacations.city}</p>
+                <p>{vacations.description}</p>
+                <p>{vacations.address}</p>
+                <p>{vacations.facilities}</p>
+                <p>{vacations.price}</p>
+                <p>{vacations.price_discount}</p>
+                <p>{vacations.rating}</p>
+                <p>{vacations.province}</p>
+                <p>{vacations.total_reviews}</p>
+                <div dangerouslySetInnerHTML={{ __html: vacations.location_maps }} />
+                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Edit Vacations
+                </button>
+                <button type="button" className="btn btn-danger">Delete</button>
+            </div>
+        </DashboardLayout>
     )
 }
