6a92ff88457b7dcaaa834e0e7a0ba0149cde5f01
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 07:25:50 +0700

    create dashboard banner page

---
+++ src/pages/dashboard/banner/index.js
@@ -0,0 +1,9 @@
+import DashboardLayout from "@/Components/DashboardLayout";
+
+export default function Banner() {
+    return (
+        <DashboardLayout>
+            <h1>Banner</h1>
+        </DashboardLayout>
+    )
+}
