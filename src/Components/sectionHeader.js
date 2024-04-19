6c5d283caf60d29c424c7ce6eb18066a21b0748b
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 15:35:50 +0700

    add link in sectionheader

--- src/Components/sectionHeader.js
+++ src/Components/sectionHeader.js
@@ -1,7 +1,17 @@
 export default function SectionHeader() {
     return (
         <section>
-            <h1>href</h1>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="/category">Category</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/promo">Promo</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/activity">Vacations</a>
+                </li>
+            </ul>
         </section>
     )
 }
