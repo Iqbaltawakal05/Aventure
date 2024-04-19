322478232b7eea640aa7fc8dad5ab377249d70b2
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 15:30:47 +0700

    change navbar code and navbar css

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,26 +2,13 @@
 export default function Navbar() {
 
     return (
-        <nav className="navbar navbar-expand-lg" id="navbar">
+        <nav className="container navbar navbar-expand-lg" id="navbar">
             <div className="container-fluid">
-                <a className="navbar-brand" href="/">Aven<span className="ture">ture</span></a>
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="/category">Category</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/promo">Promo</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/activity">Vacations</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href='/auth/login' className="nav-link">Login</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href='/auth/register' className="nav-link">Register</a>
-                    </li>
-                </ul>
+                <a className="navbar-brand" href="/">Aventure</a>
+                    <div className="auth-nav">
+                        <a href='/auth/login' className="nav-link nav-login">Login</a>
+                        <a href='/auth/register' className="nav-link nav-register">Register</a>
+                    </div>
             </div>
         </nav>
     )
