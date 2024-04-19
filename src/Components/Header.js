09c8b483606f2b2cdcf8316e7da640560b3edec9
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-19 14:42:55 +0700

    call banner.png to Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,13 +2,8 @@ export default function Header() {
     return (
         <header className="container">
             <div className="headerImg">
-                <img src="/header.jpg" alt="placeholder" />
+                <img src="/Banner.png" alt="placeholder" />
             </div>
-            <h1 className="col">Exciting adventure<div className="subhead"> awaits you!</div></h1>
-            <form className="search d-flex" role="search">
-              <input className="form-control" type="search" placeholder="search destinations" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
         </header>
     )
 }
