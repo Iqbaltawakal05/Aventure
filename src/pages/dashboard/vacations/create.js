4858d673e87f338b33ec5c11ee6ebe6787a787b0
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-23 09:00:26 +0700

    import uploadImg and add logic in create vacations

--- src/pages/dashboard/vacations/create.js
+++ src/pages/dashboard/vacations/create.js
@@ -1,12 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { createActivity } from '@/API/ActivityAPI';
+import { UploadImg } from '@/API/UploadImgAPI';
+import { fetchAllCategoriesData } from '@/API/CategoryAPI';
 
 export default function CreateActivity() {
+    const [categories, setCategories] = useState([]);
     const [formData, setFormData] = useState({
         title: '',
         description: '',
-        imageUrls: '',
+        categoryId: '',
+        imageUrls: [],
+        imageFiles: null,
         price: '',
         price_discount: '',
         rating: '',
@@ -20,8 +25,30 @@ export default function CreateActivity() {
 
     const router = useRouter();
 
+     useEffect(() => {
+        async function fetchData() {
+            const categoriesData = await fetchAllCategoriesData();
+            if (categoriesData) {
+                setCategories(categoriesData);
+            }
+        }
+        fetchData();
+    }, []);
+
     const handleChange = (e) => {
-        const { name, value } = e.target;
+        const { name, value, files } = e.target;
+        if (name === 'imageUrls') {
+            const file = files[0];
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onloadend = () => {
+                setFormData({
+                    ...formData,
+                    imageUrls: reader.result,
+                    imageFiles: file
+                });
+            };
+        }
         setFormData({
             ...formData,
             [name]: name === 'price' || name === 'price_discount' ? parseFloat(value) : value
@@ -31,13 +58,32 @@ export default function CreateActivity() {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if (!formData.title || !formData.description || !formData.imageUrls || !formData.price || !formData.price_discount || !formData.rating || !formData.total_reviews || !formData.facilities || !formData.address || !formData.province || !formData.city || !formData.location_maps) {
+        if (!formData.title || !formData.description || !formData.imageUrls || !formData.price || !formData.price_discount || !formData.rating || !formData.total_reviews || !formData.facilities || !formData.address || !formData.province || !formData.city || !formData.location_maps || !formData.categoryId) {
             console.error('Semua kolom harus diisi.');
             return;
         }
 
         try {
-            await createActivity(formData);
+
+            const uploadResponse = await UploadImg(formData.imageFiles);
+            const imageUrls = uploadResponse.data.url;
+            const activityData = {
+                title: formData.title,
+                description: formData.description,
+                categoryId: formData.categoryId,
+                imageUrls: [imageUrls],
+                price: formData.price,
+                price_discount: formData.price_discount,
+                rating: formData.rating,
+                total_reviews: formData.total_reviews,
+                facilities: formData.facilities,
+                address: formData.address,
+                province: formData.province,
+                city: formData.city,
+                location_maps: formData.location_maps
+            };
+
+            await createActivity(activityData);
             router.push('/dashboard/vacations', '/');
         } catch (error) {
             console.error('Error creating vacations:', error);
@@ -52,8 +98,16 @@ export default function CreateActivity() {
                 <input type="text" id="title" name="title" value={formData.title} onChange={handleChange} /><br />
                 <label htmlFor="description">Description:</label><br />
                 <textarea id="description" name="description" value={formData.description} onChange={handleChange}></textarea><br />
+                <label htmlFor="categoryId">Category:</label><br />
+                <select id="categoryId" name="categoryId" value={formData.categoryId} onChange={handleChange}>
+                    <option value="">Select Category</option>
+                    {categories.map(category => (
+                        <option key={category.id} value={category.id}>{category.name}</option>
+                    ))}
+                </select><br />
                 <label htmlFor="imageUrl">Image URL:</label><br />
-                <input type="text" id="imageUrl" name="imageUrls" value={formData.imageUrls} onChange={handleChange} /><br />
+                <input type="file" id="imageUrls" name="imageUrls" onChange={handleChange} /><br />
+                {formData.imageUrls && <img src={formData.imageUrls} alt="Preview" style={{ maxWidth: '200px' }} />}
                 <label htmlFor="price">Price:</label><br />
                 <input type="number" id="price" name="price" value={formData.price} onChange={handleChange} /><br />
                 <label htmlFor="price_discount">Price Discount:</label><br />
