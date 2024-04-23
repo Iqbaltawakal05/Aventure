11a1b11b4afb9a11f4e7c6dedb79ab6704c2d71b
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-23 10:54:20 +0700

    change code in Dashnav

--- src/Components/Dashnav.js
+++ src/Components/Dashnav.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import { loggedUserData } from '@/API/UserAPI';
 
-function DashNav() {
-    const [userData, setUserData] = useState(null);
+export default function DashNav() {
+    const [loggeduser, setloggeduser] = useState(null);
 
     useEffect(() => {
         async function fetchData() {
             try {
-                const userDataResponse = await loggedUserData();
-                setUserData(userDataResponse);
+                const LoggedUser = await loggedUserData();
+                setloggeduser(LoggedUser);
             } catch (error) {
                 console.error('Error fetching user data:', error);
             }
@@ -19,14 +19,12 @@ function DashNav() {
 
     return (
         <div>
-            {userData && (
+            {loggeduser && (
                 <div>
-                    <img src={userData.profilePictureUrl} alt="profile picture" />
-                    <h1>{userData.name}</h1>
+                    <img src={loggeduser.profilePictureUrl} alt="profile picture" />
+                    <h1>{loggeduser.name}</h1>
                 </div>
             )}
         </div>
     );
 }
-
-export default DashNav;
