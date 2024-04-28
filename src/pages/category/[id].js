85e0c766471e92d93ea0960321072b84ef29b331
iqbaltawakal12 <iqbaltawakall004@gmail.com>
2024-04-28 12:16:46 +0700

    success call activityByCategoryId in category

--- src/pages/category/[id].js
+++ src/pages/category/[id].js
@@ -1,4 +1,5 @@
 import { fetchCategoryById } from "@/API/CategoryAPI";
+import { fetchActivityByCategoryId } from "@/API/ActivityAPI";
 import Layout from "@/Components/Layout";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -8,33 +9,80 @@ export default function CategoryDetail() {
     const router = useRouter();
     const { id } = router.query;
     const [category, setCategory] = useState({});
+    const [activities, setActivities] = useState([]);
 
     useEffect(() => {
-        async function fetchBanner() {
+        async function fetchData() {
             try {
                 const categoryData = await fetchCategoryById(id);
                 setCategory(categoryData);
+
+                const activitiesData = await fetchActivityByCategoryId(id);
+                setActivities(activitiesData);
             } catch (error) {
-                console.error("Error fetching category data:", error);
+                console.error("Error fetching data:", error);
             }
         }
 
         if (id) {
-            fetchBanner();
+            fetchData();
         }
     }, [id]);
+
+    const formatPrice = (price) => {
+        let priceString = price.toString();
+        let priceArray = priceString.split('');
+        let formattedPrice = '';
+
+        for (let i = 0; i < priceArray.length; i++) {
+            if ((priceArray.length - i) % 3 === 0 && i !== 0) {
+                formattedPrice += '.';
+            }
+            formattedPrice += priceArray[i];
+        }
+
+        formattedPrice = 'Rp ' + formattedPrice;
+
+        return formattedPrice;
+    };
 
     return (
         <div>
             {category && (
                 <Layout>
-                    <img src={category.imageUrl} alt={category.name} />
-                    <h1>{category.name}</h1>
-                    <Link href={"/category"}>
-                    <button>Back</button>
-                    </Link>
+                    <div className="container">
+                        <div className="back-button">
+                            <a href="/category"><i class="bi bi-chevron-left"></i></a>
+                            <a href="/category">{category.name}</a>
+                        </div>
+                        {activities.length > 0 ? (
+                            <div className="row">
+                                {activities.map(activity => (
+                                    <div className="col-md-4" key={activity.id}>
+                                        <div className="mb-4">
+                                            <img src={activity.imageUrls} className="card-img" alt="..." />
+                                            <div className="card-body">
+                                                <h5 className="card-title">{activity.title}</h5>
+                                                <div className="location-vacations">
+                                                    <p className="city-vacations">{activity.city}</p>
+                                                    <p className="province-vacations">{activity.province}</p>
+                                                </div>
+                                                <div className="price">
+                                                    <p className="original-price">{formatPrice(activity.price)}</p>
+                                                    <p className="discounted-price">{formatPrice(activity.price_discount)}</p>
+                                                </div>
+                                            </div>
+                                            <Link href={`/activity/${activity.id}`}><button className="activitys-button">See Detail</button></Link>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        ) : (
+                            <p>No vacation content available for this category.</p>
+                        )}
+                    </div>
                 </Layout>
             )}
-        </div>
+            </div>
     );
 }
